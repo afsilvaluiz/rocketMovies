5be946a4f2bed664995b3abdcc9ccd5d5aa12c76
luiz <afsilvaluiz@gmail.com>
2024-01-17 09:31:42 -0300

    Header component created.

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,14 @@
 import { Container } from './styles'
 
+import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 
 export function Details() {
 
   return(
     <Container>
+      <Header />
       <h1>Hello World</h1>
-      <Button title='Login' loading/>
-      <Button title='SignUp'/>
       <Button title='Back'/>
     </Container>
   )
