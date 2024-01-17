286eb4ba0e2fe723b0ea00b357c16f1ffc7104ca
luiz <afsilvaluiz@gmail.com>
2024-01-17 07:59:42 -0300

    update files

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+  COLORS: {
+    BACKGROUND_1000: "#191E28",
+    BACKGROUND_900: "#042331",
+    BACKGROUND_800: "#074D5E",
+    BACKGROUND_700: "#7EC2C9",
+
+    WHITE: "#F4EDE8",
+    GRAY_100: "#82858B",
+
+    ROSE: "#FF859B",
+  }
+}
