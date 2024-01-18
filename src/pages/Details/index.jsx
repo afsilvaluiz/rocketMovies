a7a4597ec50285c497585550b5eeaa026ec3e3f4
luiz <afsilvaluiz@gmail.com>
2024-01-17 21:26:39 -0300

    Details updated

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,12 +2,15 @@ import { Container } from './styles'
 
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
+import { Section } from '../../components/Section'
 
 export function Details() {
 
   return(
     <Container>
       <Header />
+      <Section title="New Movie">
+      </Section>
       <Button title='Back'/>
     </Container>
   )
