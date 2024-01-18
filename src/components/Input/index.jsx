4f68b387fa332f565fb322409359ad57b2a2b14e
luiz <afsilvaluiz@gmail.com>
2024-01-17 21:23:26 -0300

    Input component created

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from './style'
+
+export function Input ({ icon: Icon, ...rest }) {
+  return (
+    <Container>
+      {Icon && <icon size={20} />}
+      <input {...rest} />
+    </Container>
+  )
+}
---
+++ src/components/Input/style.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+  border-radius: 0.625rem;
+
+  > input {
+    height: 3.5rem;
+    width: 100%;
+
+    padding: 1.188rem 1.5rem ;
+
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    background: transparent;
+    border: 0;
+
+    &:placeholder {
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+
+    > input:ready-only {
+      cursor: not-allowed;
+    }
+  }
+
+  svg {
+  margin-left: 16px;
+}
+
+
+
+`
