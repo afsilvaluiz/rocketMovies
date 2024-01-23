e9d244afeceb431f6a2667a6c805e479fa82eadb
luiz <afsilvaluiz@gmail.com>
2024-01-23 18:30:06 -0300

    Udpate main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 
+import { Details } from './Views/pages/Details';
 import { Home } from './Views/pages/Home';
 import GlobalStyles from './Views/styles/global';
 import theme from './Views/styles/theme';
@@ -10,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <Details />
     </ThemeProvider>
   </React.StrictMode>,
 );
