61645075e4ef171be3ccddd9acd80b0512412f81
luiz <afsilvaluiz@gmail.com>
2024-01-24 13:58:27 -0300

    Udpate main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 );
