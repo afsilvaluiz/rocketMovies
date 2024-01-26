8a21bbbc5e7fc96fb583037e43e2e0bfc5395403
luiz <afsilvaluiz@gmail.com>
2024-01-26 17:51:40 -0300

    MoviesItem component create

---
+++ src/Views/components/MoviesItem/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+
+  background-color: ${({ theme, isNew }) =>
+    isNew ? 'transparent' : theme.COLORS.BACKGROUND_800};
+  border: ${({ theme, isNew }) =>
+    isNew ? `1px dashed ${theme.COLORS.GRAY_100}` : 'none'};
+
+  border-radius: 1rem;
+  padding-right: 1rem;
+
+  > button {
+    border: none;
+    background: none;
+
+    color: ${({ theme }) => theme.COLORS.ROSE};
+  }
+
+  > input {
+    width: 100%;
+
+    padding: 1rem;
+
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    background: transparent;
+
+    border: none;
+
+    &::placeholder {
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+  }
+`;
