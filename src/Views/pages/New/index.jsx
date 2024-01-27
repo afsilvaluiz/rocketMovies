dbd5f7349aad156b3ca6f38685a7dbc899a5e5e1
luiz <afsilvaluiz@gmail.com>
2024-01-27 18:27:02 -0300

    routes configured

--- src/Views/pages/New/index.jsx
+++ src/Views/pages/New/index.jsx
@@ -1,4 +1,5 @@
 import { FiArrowLeft } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import { Button } from '../../components/Button';
 import { ButtonText } from '../../components/ButtonText';
@@ -18,9 +19,9 @@ export function New() {
       <main>
         <Form>
           <header>
-            <a href='#'>
+            <Link to='/'>
               <ButtonText icon={FiArrowLeft} title='Back' />
-            </a>
+            </Link>
 
             <h1>New Movie</h1>
           </header>
