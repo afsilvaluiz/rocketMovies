3b4668a3a712838e0229153ec0f19cee404d0775
luiz <afsilvaluiz@gmail.com>
2024-02-01 12:35:01 -0300

    feat: loading user data.

--- src/Views/pages/Profile/index.jsx
+++ src/Views/pages/Profile/index.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import { FiArrowLeft, FiLock, FiMail, FiUser, FiCamera } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
+import { useAuth } from '../../../App/hooks/auth';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 
 import { Container, Form, Avatar } from './styles';
 
 export function Profile() {
+  const { user } = useAuth();
+
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [passwordOld, setPasswordOld] = useState();
+  const [passwordNew, setPasswordNew] = useState();
+
   return (
     <Container>
       <header>
@@ -26,13 +35,35 @@ export function Profile() {
           </label>
         </Avatar>
 
-        <Input placeholder='Name' type='text' icon={FiUser} />
+        <Input
+          placeholder='Name'
+          type='text'
+          icon={FiUser}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-        <Input placeholder='E-mail' type='text' icon={FiMail} />
+        <Input
+          placeholder='E-mail'
+          type='text'
+          icon={FiMail}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-        <Input placeholder='Current password' type='password' icon={FiLock} />
+        <Input
+          placeholder='Current password'
+          type='password'
+          icon={FiLock}
+          onChange={(e) => setPasswordOld(e.target.value)}
+        />
 
-        <Input placeholder='New password' type='password' icon={FiLock} />
+        <Input
+          placeholder='New password'
+          type='password'
+          icon={FiLock}
+          onChange={(e) => setPasswordNew(e.target.value)}
+        />
 
         <Button title='Save' />
       </Form>
