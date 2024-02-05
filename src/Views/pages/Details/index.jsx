9d8fe0236c61f9075dd1b375f30e95463d51922b
luiz <afsilvaluiz@gmail.com>
2024-02-05 11:50:19 -0300

    feat: load movie details.

--- src/Views/pages/Details/index.jsx
+++ src/Views/pages/Details/index.jsx
@@ -1,82 +1,98 @@
+import moment from 'moment-timezone';
+import { useEffect, useState } from 'react';
 import { FiArrowLeft, FiClock, FiDelete } from 'react-icons/fi';
+import { useNavigate, useParams } from 'react-router-dom';
 
+import { useAuth } from '../../../App/hooks/auth';
+import { api } from '../../../App/services/api';
 import { ButtonText } from '../../components/ButtonText';
 import { Header } from '../../components/Header';
 import { Stars } from '../../components/Stars';
 import { Tag } from '../../components/Tag';
+import avatarPlaceholder from '../../layouts/assets/avatar_placeholder.svg';
 
 import { Container, Content } from './styles';
 
 export function Details() {
+  const [data, setData] = useState(null);
+
+  const params = useParams();
+
+  const { user } = useAuth();
+
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder;
+
+  const formattedDate = data
+    ? moment(data.created_at)
+        .tz('America/Sao_Paulo')
+        .format('DD/MM/YYYY HH:mm:ss')
+    : '';
+
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate('/');
+  }
+
+  useEffect(() => {
+    async function fetchMoviesDetails() {
+      const response = await api.get(`/movie_notes/${params.id}`);
+      setData(response.data);
+    }
+
+    fetchMoviesDetails();
+  }, [params.id]);
+
   return (
     <Container>
-      <Header readerOnly />
-
-      <main>
-        <Content>
-          <header>
-            <div>
-              <a href='#'>
-                <ButtonText icon={FiArrowLeft} title='Back' />
-              </a>
-
-              <ButtonText icon={FiDelete} title='Delete Movie' />
-            </div>
-          </header>
-          <section>
-            <div>
-              <h1>Interestellar</h1>
-              <Stars />
-              <Stars />
-              <Stars />
-              <Stars />
-              <Stars />
-            </div>
-
-            <div className='movie-info'>
-              <p>
-                <img
-                  src='https://github.com/afsilvaluiz.png'
-                  alt='Profile image'
-                />
-                By Luiz Silva
-              </p>
-
+      <Header />
+      {data && (
+        <main>
+          <Content>
+            <header>
               <div>
-                <FiClock />
+                <a onClick={handleBack}>
+                  <ButtonText icon={FiArrowLeft} title='Back' />
+                </a>
 
-                <p>18/01/24 às 13:00</p>
+                <ButtonText icon={FiDelete} title='Delete Movie' />
               </div>
-            </div>
-          </section>
+            </header>
+            <section>
+              <div>
+                <h1>{data.title}</h1>
+                <Stars rating={data.rating} />
+              </div>
 
-          <div>
-            <Tag title='Drama' />
-            <Tag title='Science Fiction' />
-          </div>
+              <div className='movie-info'>
+                <p>
+                  <img src={avatarUrl} alt={user.name} />
+                  By {user.name}
+                </p>
 
-          <article>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni
-              accusamus excepturi odit totam numquam dolore architecto,
-              molestiae temporibus, dignissimos, expedita fugit veritatis soluta
-              accusantium consectetur placeat? Itaque illo harum dolor. Lorem
-              ipsum dolor, sit amet consectetur adipisicing elit. Magni
-              accusamus excepturi odit totam numquam dolore architecto,
-              molestiae temporibus, dignissimos, expedita fugit veritatis soluta
-              accusantium consectetur placeat? Itaque illo harum dolor. Lorem
-              ipsum dolor, sit amet consectetur adipisicing elit. Magni
-              accusamus excepturi odit totam numquam dolore architecto,
-              molestiae temporibus, dignissimos, expedita fugit veritatis soluta
-              accusantium consectetur placeat? Itaque illo harum dolor. Lorem
-              ipsum dolor, sit amet consectetur adipisicing elit. Magni
-              accusamus excepturi odit totam numquam dolore architecto,
-              molestiae temporibus, dignissimos, expedita fugit veritatis soluta
-              accusantium consectetur placeat? Itaque illo harum dolor.
-            </p>
-          </article>
-        </Content>
-      </main>
+                <div>
+                  <FiClock />
+
+                  <p>{formattedDate}</p>
+                </div>
+              </div>
+            </section>
+            {data.movie_tags && (
+              <div>
+                {data.movie_tags.map((tag) => (
+                  <Tag key={String(tag.id)} title={tag.name} />
+                ))}
+              </div>
+            )}
+            <article>
+              <p>{data.description}</p>
+            </article>
+          </Content>
+        </main>
+      )}
+      ;
     </Container>
   );
 }
