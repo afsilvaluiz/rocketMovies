6c99c31857f82a57035302491a7c4a4df0680095
luiz <afsilvaluiz@gmail.com>
2024-02-05 08:20:14 -0300

    refactor: change the map index.

--- src/Views/components/Stars/index.jsx
+++ src/Views/components/Stars/index.jsx
@@ -2,12 +2,16 @@ import { FiStar } from 'react-icons/fi';
 
 import { Container } from './styles';
 
-export function Stars() {
+export function Stars({ rating }) {
+  let myArray = new Array(5).fill(undefined);
+
   return (
     <Container>
-      <li className='bg'>
-        <FiStar />
-      </li>
+      {myArray.map((e, index) => (
+        <li key={String(index)} className={index + 1 <= rating ? 'bg' : ''}>
+          <FiStar />
+        </li>
+      ))}
     </Container>
   );
 }
