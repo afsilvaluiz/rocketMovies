76e446427f9ca314cde74920fc64535d4b4ea04c
luiz <afsilvaluiz@gmail.com>
2024-02-05 08:24:21 -0300

    feat: function to display movies added by the user.

--- src/Views/pages/Home/index.jsx
+++ src/Views/pages/Home/index.jsx
@@ -1,14 +1,40 @@
+import { useEffect, useState } from 'react';
 import { FiPlus } from 'react-icons/fi';
 
+import { api } from '../../../App/services/api';
 import { Header } from '../../components/Header';
 import { Movies } from '../../components/Movies';
 
 import { Container, Content, AddMovie } from './styles';
 
 export function Home() {
+  const [movie_notes, setNotes] = useState([]);
+  const [movie_tags, setTags] = useState([]);
+  const [search, setSearch] = useState('');
+  const [tagsSelected, setTagsSelected] = useState([]);
+
+  useEffect(() => {
+    async function fetchNotes() {
+      const response = await api.get(
+        `/movie_notes?title=${search}&movie_tags=${tagsSelected}`,
+      );
+      setNotes(response.data);
+    }
+
+    fetchNotes();
+  }, [tagsSelected, search]);
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get('/movie_tags');
+      setTags(response.data);
+    }
+    fetchTags();
+  }, []);
+
   return (
     <Container>
-      <Header readerOnly />
+      <Header onChange={(e) => setSearch(e.target.value)} />
 
       <main>
         <header className='myMovie'>
@@ -20,15 +46,9 @@ export function Home() {
           </AddMovie>
         </header>
         <Content>
-          <Movies
-            data={{
-              title: 'Interestellar',
-              tags: [
-                { id: '1', name: 'Drama' },
-                { id: '2', name: 'Science Fiction' },
-              ],
-            }}
-          />
+          {movie_notes.map((note) => (
+            <Movies key={String(note.id)} data={note} />
+          ))}
         </Content>
       </main>
     </Container>
