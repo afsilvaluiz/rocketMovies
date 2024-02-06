6c3647f0139907f9fb57cd936d06ec6f164ec769
luiz <afsilvaluiz@gmail.com>
2024-02-06 11:54:15 -0300

    feat: logout function.

--- src/Views/components/Header/index.jsx
+++ src/Views/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import { FiSearch } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { useAuth } from '../../../App/hooks/auth';
 import { api } from '../../../App/services/api';
@@ -10,6 +10,12 @@ import { Container, Profile, Logout } from './styles';
 
 export function Header({ ...props }) {
   const { signOut, user } = useAuth();
+  const navigation = useNavigate();
+
+  function handleSignOut() {
+    navigation('/');
+    signOut();
+  }
 
   const avatarUrl = user.avatar
     ? `${api.defaults.baseURL}/files/${user.avatar}`
@@ -33,7 +39,7 @@ export function Header({ ...props }) {
       <Profile>
         <div>
           <strong>{user.name}</strong>
-          <Logout onClick={signOut}>Logout</Logout>
+          <Logout onClick={handleSignOut}>Logout</Logout>
         </div>
         <Link to='/profile'>
           <img src={avatarUrl} alt={user.name} />
