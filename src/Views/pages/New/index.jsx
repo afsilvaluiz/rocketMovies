db7f28c70e98a97f1b2f4a1d63c7a429989bb68e
luiz <afsilvaluiz@gmail.com>
2024-02-09 09:30:58 -0300

    style: animate page transition

--- src/Views/pages/New/index.jsx
+++ src/Views/pages/New/index.jsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import { useState } from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Link, useNavigate } from 'react-router-dom';
@@ -62,73 +63,79 @@ export function New() {
   }
 
   return (
-    <Container>
-      <Header />
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 0.45 } }}
+      exit={{ opacity: 0 }}
+    >
+      <Container>
+        <Header />
 
-      <main>
-        <Form>
-          <header>
-            <Link to='/'>
-              <ButtonText icon={FiArrowLeft} title='Back' />
-            </Link>
+        <main>
+          <Form>
+            <header>
+              <Link to='/'>
+                <ButtonText icon={FiArrowLeft} title='Back' />
+              </Link>
 
-            <h1>New Movie</h1>
-          </header>
+              <h1>New Movie</h1>
+            </header>
 
-          <div className='movieRating'>
-            <Input
-              className='title'
-              placeholder='Title'
-              type='text'
-              onChange={(e) => setTitle(e.target.value)}
-            />
+            <div className='movieRating'>
+              <Input
+                className='title'
+                placeholder='Title'
+                type='text'
+                onChange={(e) => setTitle(e.target.value)}
+              />
 
-            <Input
-              className='rating'
-              placeholder='Your rating (from 0 to 5)'
-              type='number'
-              min='0'
-              max='5'
-              value={rating}
-              onChange={(e) => setRating(e.target.value)}
-            />
-          </div>
-
-          <Textarea
-            placeholder='Comments'
-            onChange={(e) => setDescription(e.target.value)}
-          />
-
-          <Section>
-            <h2>Tags</h2>
-            <div className='tags'>
-              {tags.map((tag, index) => (
-                <MoviesItem
-                  className='tag'
-                  key={String(index)}
-                  value={tag}
-                  onClick={() => handleRemoveTag(tag)}
-                />
-              ))}
-
-              <MoviesItem
-                className='newTag'
-                isNew
-                placeholder='New tag'
-                onChange={(e) => setNewTag(e.target.value)}
-                value={newTag}
-                onClick={handleAddTag}
+              <Input
+                className='rating'
+                placeholder='Your rating (from 0 to 5)'
+                type='number'
+                min='0'
+                max='5'
+                value={rating}
+                onChange={(e) => setRating(e.target.value)}
               />
             </div>
-          </Section>
 
-          <div className='saveChange'>
-            <Button title='Discard' onClick={handleBack} />
+            <Textarea
+              placeholder='Comments'
+              onChange={(e) => setDescription(e.target.value)}
+            />
 
-            <Button title='Save' onClick={handleNewMovie} />
-          </div>
-        </Form>
-      </main>
-    </Container>
+            <Section>
+              <h2>Tags</h2>
+              <div className='tags'>
+                {tags.map((tag, index) => (
+                  <MoviesItem
+                    className='tag'
+                    key={String(index)}
+                    value={tag}
+                    onClick={() => handleRemoveTag(tag)}
+                  />
+                ))}
+
+                <MoviesItem
+                  className='newTag'
+                  isNew
+                  placeholder='New tag'
+                  onChange={(e) => setNewTag(e.target.value)}
+                  value={newTag}
+                  onClick={handleAddTag}
+                />
+              </div>
+            </Section>
+
+            <div className='saveChange'>
+              <Button title='Discard' onClick={handleBack} />
+
+              <Button title='Save' onClick={handleNewMovie} />
+            </div>
+          </Form>
+        </main>
+      </Container>
+    </motion.div>
   );
 }
